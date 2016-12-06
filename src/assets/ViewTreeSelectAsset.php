02dbc5c81b8599e67f68dd9830086303287477cb
iadfeshchm <zlatov@ya.ru>
2016-12-06 12:15:29 +0300

    tree - Стиль для <select>

---
+++ src/assets/ViewTreeSelectAsset.php
@@ -0,0 +1,16 @@
+<?php
+namespace Zlatov\yiiComponents\assets;
+
+use yii\web\AssetBundle;
+
+class ViewTreeSelectAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/zlatov/yii-components/src/assets/sources/viewtreeselect/';
+    public $css = [
+        'css.css',
+    ];
+    public $js = [
+    ];
+    public $depends = [
+    ];
+}
